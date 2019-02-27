d5260468a76232beeb23b85b0924bd5b22981147
Erik Lorenz <erikslorenz@gmail.com>
2019-02-27 00:20:26 -0800

    added logic for wiping the schedule

--- src/contexts/Schedule.js
+++ src/contexts/Schedule.js
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
+import { getTime, getNow } from '../utils/getDates';
 
 export const ScheduleContext = createContext();
 
@@ -12,8 +13,33 @@ export const ScheduleProvider = ({ children, history }) => {
     room: '',
   };
 
-  const initialState = () =>
-    JSON.parse(localStorage.getItem('schedule')) || blankValues;
+  // Check if time has passed and there are old ones in there.
+  const initialState = () => {
+    const storedSchedule = JSON.parse(localStorage.getItem('schedule'));
+
+    // Check if there are values and not blank
+    if (storedSchedule.pickupHour) {
+      // Check if it's a day old
+      if (getNow().day > getTime(storedSchedule.pickupHour).day) {
+        console.log('Schedule discarded, one day past.');
+        return blankValues;
+      }
+
+      // Check if too much time has passed on same day
+      const nowHour = getNow().hour;
+      const storedHour = getTime(storedSchedule.pickupHour).hour;
+      const diff = storedHour - nowHour;
+
+      if (diff < 2) {
+        console.log('Scheduled discarded, too much time passed.');
+        return blankValues;
+      } else {
+        console.log('Previous schedule hydrated.');
+        return storedSchedule;
+      }
+    }
+    return storedSchedule || blankValues;
+  };
 
   const [schedule, setSchedule] = useState(initialState);
 
