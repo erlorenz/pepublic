7c1db4a830589b667a8c03b0fede545739fb3688
Erik Lorenz <erikslorenz@gmail.com>
2019-03-04 13:58:39 -0800

    dsfsf

--- src/pages/Garments/priceList.js
+++ src/pages/Garments/priceList.js
@@ -1,112 +1,100 @@
-const priceList = [
+const priceListUnordered = [
   {
-    id: 1,
     slug: 'suit',
     description: 'Suit',
     price: 2050,
   },
   {
-    id: 2,
     slug: 'pants',
     description: 'Pants/Slacks',
     price: 950,
   },
   {
-    id: 3,
     slug: 'blazer',
     description: 'Sports Coat/Blazer',
     price: 1250,
   },
   {
-    id: 4,
     slug: 'shirtdress',
     description: 'Dress Shirt',
     price: 900,
   },
   {
-    id: 5,
     slug: 'shirtsport',
     description: 'Sports Shirt',
     price: 900,
   },
   {
-    id: 6,
     slug: 'shirtwestern',
     description: 'Western Shirt',
     price: 1400,
   },
   {
-    id: 7,
     slug: 'blouse',
     description: 'Blouse',
     price: 1050,
   },
   {
-    id: 8,
     slug: 'vest',
     description: 'Vest',
     price: 850,
   },
   {
-    id: 9,
     slug: 'tie',
     description: 'Tie',
     price: 650,
   },
   {
-    id: 10,
     slug: 'dress',
     description: 'Dress (Plain)',
     price: 1650,
   },
   {
-    id: 11,
     slug: 'dressformal',
     description: 'Dress (Formal)',
     price: 2200,
   },
   {
-    id: 12,
     slug: 'skirt',
     description: 'Skirt (Plain)',
     price: 1250,
   },
   {
-    id: 13,
     slug: 'skirtlong',
     description: 'Skirt (Long)',
     price: 1500,
   },
   {
-    id: 14,
     slug: 'sweater',
     description: 'Sweater',
     price: 1200,
   },
   {
-    id: 15,
     slug: 'tuxedo',
     description: 'Tuxedo - 2 piece',
     price: 2500,
   },
   {
-    id: 16,
     slug: 'shirtformal',
     description: 'Formal Shirt',
     price: 1100,
   },
   {
-    id: 17,
     slug: 'shorts',
     description: 'Shorts',
     price: 800,
   },
   {
-    id: 18,
     slug: 'coat',
     description: 'Coat/Raincoat',
     price: 2200,
   },
 ];
+
+// Add an ID to each for calculations
+const priceList = priceListUnordered.map((x, i) => {
+  x.id = i + 1;
+  return x;
+});
 
 export default priceList;
